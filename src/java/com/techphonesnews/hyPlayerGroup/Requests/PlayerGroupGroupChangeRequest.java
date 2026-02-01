f7eaf96577e5ab53af3d790d41724e464b55980e
pro470 <reynoldsadjei@gmx.de>
2026-02-01 16:19:40 +0100

    fix: forgot the interface ups...

--- src/java/com/techphonesnews/hyPlayerGroup/Requests/PlayerGroupGroupChangeRequest.java
+++ src/java/com/techphonesnews/hyPlayerGroup/Requests/PlayerGroupGroupChangeRequest.java
@@ -1,16 +1,18 @@
 package com.techphonesnews.hyPlayerGroup.Requests;
 
+import com.techphonesnews.hyPlayerGroup.Group.PlayerGroupAffected;
 import com.techphonesnews.hyPlayerGroup.Group.PlayerGroupDAG;
 
-sealed public interface PlayerGroupGroupChangeRequest permits AddGroupParentRequest,
-        RemoveGroupParentRequest,
-        AddPlayerToGroupRequest,
-        RemovePlayerFromGroupRequest,
-        AddPlayerPermissionRequest,
-        RemovePlayerPermissonRequest,
-        AddGroupPermissonRequest,
-        RemoveGroupPermissionRequest {
+import javax.annotation.Nonnull;
+
+sealed public interface PlayerGroupGroupChangeRequest permits AddGroupParentRequest, AddGroupPermissonRequest, AddPlayerPermissionRequest, AddPlayerToGroupRequest, CreateGroupRequest, DisbandGroupRequest, RemoveGroupParentRequest, RemoveGroupPermissionRequest, RemovePlayerFromGroupRequest, RemovePlayerPermissonRequest {
     void apply(PlayerGroupDAG dag);
 
     void event();
+
+    String debugMessage();
+
+    @Nonnull
+    PlayerGroupAffected affected();
+
 }
